cd4156321f20ababdc6f7360c9ec4e7653507e9c
Mark Szymczyk <mark@meandmark.com>
2018-02-28 23:29:42 -0500

    Add code to move the player.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    float speed;
+
 	// Use this for initialization
 	void Start () {
-		
+        speed = 10.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Move();
+	}
+
+	public void Move() {
+		float xMovement = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.Translate(Vector3.right * xMovement);
+
+        float yMovement = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(Vector3.up * yMovement);
 	}
 }
